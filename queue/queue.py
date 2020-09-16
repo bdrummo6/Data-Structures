6b4796ec7e79c3ac4f72406bcce99392761a7302
bdrummo6 <bradley.drummonds@gmail.com>
2020-09-16 00:48:15 -0400

    Completed implementation of a queue
    I implemented using an array and linked list. All tests passed.

--- queue/queue.py
+++ queue/queue.py
@@ -13,16 +13,76 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
+class Node:
+    def __init__(self, value, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+    def get_value(self):
+        return self.value
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        self.next_node = new_next
+
+
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
-    
+        self.head = None
+        self.tail = None
+
     def __len__(self):
-        pass
+        if self.head is None:
+            self.tail = None
+            return 0
+        else:
+            self.size = 1
+            current_node = self.head
+            while current_node.get_next() is not None:
+                self.size += 1
+                current_node = current_node.get_next()
+            return self.size
 
     def enqueue(self, value):
-        pass
+        new_node = Node(value)
+        value = new_node.get_value()
+        if self.tail is None:
+            self.head = self.tail = new_node
+            return value
+        else:
+            self.tail.set_next(new_node)
+            self.tail = new_node
+            return value
 
     def dequeue(self):
-        pass
+        if self.head is None and self.tail is None:
+            return None
+        else:
+            value = self.head.get_value()
+            exit_node = self.head
+            self.head = exit_node.get_next()
+            return value
+
+
+"""
+# Implemented using an array
+class Queue:
+    def __init__(self):
+        self.size = 0
+        self.storage = []
+    
+    def __len__(self):
+        return len(self.storage)
+
+    def enqueue(self, value):
+        self.storage.insert(0, value)
+
+    def dequeue(self):
+        if len(self.storage) == 0:
+            return None
+        return self.storage.pop()
+"""
