9cd3f3cd18e445bf8e11c929c308ec79f2630c73
bdrummo6 <bradley.drummonds@gmail.com>
2020-09-18 15:55:52 -0400

    Update singly_linked_list.py

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -1,17 +1,17 @@
 
 class Node:
-    def __init__(self, value, next_node=None):
+    def __init__(self, value, next=None):
         self.value = value
-        self.next_node = next_node
+        self.next = next
 
     def get_value(self):
         return self.value
 
     def get_next(self):
-        return self.next_node
+        return self.next
 
     def set_next(self, new_next):
-        self.next_node = new_next
+        self.next = new_next
 
 class LinkedList:
     def __init__(self):
