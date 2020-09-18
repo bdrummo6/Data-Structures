3f794088db12749c89a00921d4fd9f8c4aa2c750
bdrummo6 <bradley.drummonds@gmail.com>
2020-09-18 15:50:23 -0400

    Added getter and setter functions

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -10,6 +10,21 @@ class ListNode:
         self.value = value
         self.next = next
 
+    def get_value(self):
+        return self.value
+
+    def get_prev(self):
+        return self.prev
+
+    def set_prev(self, new_prev):
+        self.prev = new_prev
+
+    def get_next(self):
+        return self.next
+
+    def set_next(self, new_next):
+        self.next = new_next
+
 
 """
 Our doubly-linked list class. It holds references to
@@ -36,11 +51,11 @@ class DoublyLinkedList:
         new_node = ListNode(value)
         if not self.head:
             self.head = self.tail = new_node
-            self.head.prev = None
+            self.head.set_prev(None)
             self.length += 1
         else:
-            new_node.next = self.head
-            new_node.prev = None
+            new_node.set_next(self.head)
+            new_node.set_prev(None)
             self.head = new_node
             self.length += 1
 
@@ -53,17 +68,16 @@ class DoublyLinkedList:
     """
 
     def remove_from_head(self):
+        value = self.head.get_value()
         # check for empty list
         if not self.head:
             return None
         elif self.length == 1:
-            value = self.head.value
             self.head = self.tail = None
             self.length -= 1
         else:
-            value = self.head.value
-            self.head = self.head.next
-            self.head.prev = None
+            self.head = self.head.get_next()
+            self.head.set_prev(None)
             self.length -= 1
 
         return value
@@ -81,13 +95,13 @@ class DoublyLinkedList:
             self.length += 1
         else:
             curr_node = self.head
-            while curr_node.next:
-                curr_node = curr_node.next
+            while curr_node.get_next():
+                curr_node = curr_node.get_next()
 
-            curr_node.next = new_node
-            self.tail = curr_node.next
-            self.tail.next = None
-            self.tail.prev = curr_node
+            curr_node.set_next(new_node)
+            self.tail = curr_node.get_next()
+            self.tail.set_next(None)
+            self.tail.set_prev(curr_node)
             self.length += 1
 
         return value
@@ -104,19 +118,19 @@ class DoublyLinkedList:
             return None
         # check if there is only one node
         elif self.length == 1:
-            value = self.tail.value
+            value = self.tail.get_value()
             self.head = self.tail = None
             self.length -= 1
         else:
-            value = self.tail.value
+            value = self.tail.get_value()
             current_node = self.head
 
-            while current_node.next:
+            while current_node.get_next():
                 # keep looping
-                current_node = current_node.next
+                current_node = current_node.get_next()
 
-            self.tail = current_node.prev
-            self.tail.next = None
+            self.tail = current_node.get_prev()
+            self.tail.set_next(None)
             self.length -= 1
 
         return value
@@ -130,7 +144,7 @@ class DoublyLinkedList:
         if self.length == 0:
             return None
         else:
-            value = node.value
+            value = node.get_value()
             self.delete(node)
             self.add_to_head(value)
 
@@ -145,7 +159,7 @@ class DoublyLinkedList:
         if self.length == 0:
             return None
         else:
-            value = node.value
+            value = node.get_value()
             self.delete(node)
             self.add_to_tail(value)
 
@@ -157,28 +171,28 @@ class DoublyLinkedList:
     """
 
     def delete(self, node):
-        value = node.value
+        value = node.get_value()
         if self.length == 0:
             return None
-        elif self.head.value == value:
+        elif self.head.get_value() == value:
             self.remove_from_head()
         else:
             curr_node = self.head
-            prev_node = curr_node.prev
-            while curr_node.next:
-                if curr_node.value == value:
+            prev_node = curr_node.get_prev()
+            while curr_node.get_next():
+                if curr_node.get_value() == value:
                     break
                 prev_node = curr_node
-                curr_node = curr_node.next
+                curr_node = curr_node.get_next()
 
             # Check if node with value exist in the list
-            if curr_node.value != value:
+            if curr_node.get_value() != value:
                 return None
-            elif not curr_node.next:
+            elif not curr_node.get_next():
                 self.remove_from_tail()
             else:
-                prev_node.next = curr_node.next
-                prev_node.prev = curr_node.prev
+                prev_node.set_next(curr_node.get_next())
+                prev_node.set_prev(curr_node.get_prev())
                 self.length -= 1
 
         return value
@@ -189,17 +203,57 @@ class DoublyLinkedList:
     """
 
     def get_max(self):
-        max_value = self.head.value
-        curr_node = self.head.next
+        max_value = self.head.get_value()
+        curr_node = self.head.get_next()
         while curr_node:
-            if max_value < curr_node.value:
-                max_value = curr_node.value
-            curr_node = curr_node.next
+            if max_value < curr_node.get_value():
+                max_value = curr_node.get_value()
+            curr_node = curr_node.get_next()
 
         return max_value
 
     def print_list(self):
         curr_node = self.head
         while curr_node:
-            print(curr_node.value)
-            curr_node = curr_node.next
+            print(curr_node.get_value())
+            curr_node = curr_node.get_next()
+
+
+"""
+dll = DoublyLinkedList()
+
+# add_to_head
+dll.add_to_head(7)  # 7
+dll.add_to_head(12)  # 12 7
+dll.add_to_head(23)  # 23 12 7
+dll.add_to_head(35)  # 35 23 12 7
+dll.add_to_head(54)  # 54 35 23 12 7
+
+# remove_from_head
+dll.remove_from_head()  # 35 23 12 7
+
+# add_to_tail
+dll.add_to_tail(16)  # 35 23 12 7 16
+dll.add_to_tail(45)  # 35 23 12 7 16 45
+dll.add_to_tail(76)  # 35 23 12 7 16 45 76
+
+# remove_from_tail
+dll.remove_from_tail()  # 35 23 12 7 16 45
+
+# move_to_front
+dll.move_to_front(ListNode(16))  # 16 35 23 12 7 45
+
+# move_to_back
+dll.move_to_end(ListNode(23))  # 16 35 12 7 45 23
+
+# delete
+dll.delete(ListNode(12))  # 16 35 7 45 23
+
+dll.print_list()  # 16 35 7 45 23
+
+# list max value
+print('\nMax value: ' + str(dll.get_max()))  # 45
+
+# list length
+print('\nList Length: ' + str(len(dll)))  # 5
+"""
