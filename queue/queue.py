f05f2870b5ba0f3d973e0af33753aca461b97718
bdrummo6 <bradley.drummonds@gmail.com>
2020-09-21 20:32:42 -0400

    Update queue.py

--- queue/queue.py
+++ queue/queue.py
@@ -3,12 +3,12 @@ A queue is a data structure whose primary purpose is to store and
 return elements in First In First Out(FIFO) order.
 
 1. Implement the Queue class using an list as the underlying storage structure.
-   Make sure the Queue tests pass.
-2. Re-implement the Queue class, this time using the linked list implementation
-   as the underlying storage structure.
-   Make sure the Queue tests pass.
-3. What is the difference between using an list vs. a linked list when
-   implementing a Queue?
+   Make sure the Queue tests pass. (Complete)
+
+2. Re-implement the Queue class, this time using the linked list implementation as the underlying storage structure.
+   Make sure the Queue tests pass.  (Complete)
+
+3. What is the difference between using an list vs. a linked list when implementing a Queue?
    
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
