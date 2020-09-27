68c88d006cc1c7d11d19d02d2de63b38a6e2ab0a
bdrummo6 <bradley.drummonds@gmail.com>
2020-09-27 14:03:19 -0400

    Update max_heap.py
    Created insert function

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,9 +1,21 @@
+
 class Heap:
     def __init__(self):
         self.storage = []
 
     def insert(self, value):
-        pass
+        if self.storage:
+            for val in self.storage:
+                if val > value:
+                    if not self.storage.index(val):
+                        self.storage.append(value)
+                    else:
+                        self.insert(value)
+                elif value > val:
+                    self.storage.insert(self.storage.index(val), value)
+
+        else:
+            self.storage.append(value)
 
     def delete(self):
         pass
@@ -19,3 +31,15 @@ class Heap:
 
     def _sift_down(self, index):
         pass
+
+mh = Heap()
+
+mh.insert(11)
+mh.insert(9)
+mh.insert(17)
+mh.insert(12)
+
+print(mh.storage)
+
+
+
